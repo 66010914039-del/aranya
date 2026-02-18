aeecd92f49f6f86a87b53e9d121bf83c0aae146a
66010914039-del <66010914039@msu.ac.th>
2026-02-18 17:11:18 +0700

    a

--- j/a.php
+++ j/a.php
@@ -1,11 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>งาน jอรัญญา เนื่องคำอินทร์ (เกด)</title>
+    <meta charset="UTF-8">
+    <title>งาน k</title>
 </head>
-
 <body>
-    <h1>งาน jอรัญญา เนื่องคำอินทร์ (เกด) </h1><img src = "images/1.jpg" width = "200">
+
+    <h2>
+        งาน k <br>
+        รหัสนิสิต: 66010914039 <br>
+        ชื่อ-สกุล: อรัญญา เนื่องคำอินทร์
+    </h2>
+
+    <br>
+
+    <!-- ปุ่มเปิดรูปตัวเอง -->
+    <button style="background-color: green; color: white; padding:10px;"
+            onclick="window.open('myphoto.jpg')">
+        เปิดรูปตัวเอง
+    </button>
+
+    <br><br>
+
+    <!-- ปุ่มเปิดรูปอาจารย์ -->
+    <button style="background-color: orange; color: white; padding:10px;"
+            onclick="window.open('teacher.jpg')">
+        เปิดรูปอาจารย์ผู้สอน
+    </button>
+
 </body>
 </html>
