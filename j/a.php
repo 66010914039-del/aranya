fd43815dc7115b6ea9d5a0e446a8c969a03a4cd2
66010914039-del <66010914039@msu.ac.th>
2026-02-18 15:27:43 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>งาน jอรัญญา เนื่องคำอินทร์ (เกด)</title>
+</head>
+
+<body>
+    <h1>งาน jอรัญญา เนื่องคำอินทร์ (เกด) </h1><img src = "images/1.jpg" width = "200">
+</body>
+</html>
